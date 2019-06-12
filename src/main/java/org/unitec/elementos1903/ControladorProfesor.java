b78bb8a6feb6ca7ce3d6d8d8cc4eab4759f31c42
Servicios Especiales <Servicios Especiales@SE02>
2019-06-11 21:35:01 -0500

    Arq Proyecto Final

---
+++ src/main/java/org/unitec/elementos1903/ControladorProfesor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elementos1903;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Servicios Especiales
+ */
+@RestController
+@RequestMapping("/api")
+public class ControladorProfesor {
+    
+    //METODO PARA GUARDAR 
+    @PostMapping("/profesor")
+    public Estatus guardar(@RequestBody String json)throws Exception{
+        
+    }
+    
+    
+}
